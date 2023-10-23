d9223b89932c32b75e737029f90b4d97b9213d59
Byron Salvador <byron.salvador@epn.edu.ec>
2023-10-23 18:55:24 -0500

    Feat: integración con la bdd PlayFab
    Se añade la funcionalidad de login y signin con usuario, contraseña y correo.

---
+++ Assets/PlayFab/PlayFabConstants.cs
@@ -0,0 +1,6 @@
+namespace PlayfabTutorial.Scripts.PlayFab {
+    public static class PlayFabConstants {
+        public static string TitleID = "BA04A";
+        public static string SavedUsername = "SAVED_USERNAME";
+    }
+}
